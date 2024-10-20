0d22fe34a0c8ebf7973d1d4bdbb30b86942edcc6
Lionel Work <lionelwork@Lionels-MBP.lan>
2024-10-20 16:47:59 +0800

    added foundation code

---
+++ src/pages/landingPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const LandingPage = ({navigation, route }: any) => {
+
+
+
+    return (
+        <>
+        
+        </>
+    )
+
+}
+
+export default LandingPage;
